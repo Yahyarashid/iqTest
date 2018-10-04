380b69a0205d94679e22688f79a8e9ec4f92e45f
IssaAnagreh <isa_anagreh@hotmail.com>
2018-10-04 09:36:04 +0300

    Day 2 start

--- main.js
+++ main.js
@@ -29,57 +29,92 @@ var q8 = Question('Mary, who is sixteen years old, is four times as old as her b
 var q9 = Question('Which one of the numbers does not belong in the following series? \n 2 - 3 - 6 - 7 - 8 - 14 - 15 - 30', [3,7,8,15,30], 8);
 var q10 = Question('Which one of the five choices makes the best comparison? \n Finger is to Hand as Leaf is to:', ['Twig','Tree','Branch','Blossom','Bark'],'Twig');
 var q11 = Question('If you rearrange the letters "CIFAIPC" you would have the name of a(n):', ['City','Animal','Ocean','River','Country'], 'Ocean');
-var q12 = Question('Choose the number that is 1/4 of 1/2 of 1/5 of 200:');
 var q12 = Question('If all Bloops are Razzies and all Razzies are Lazzies, then all Bloops are definitely Lazzies?',[true,false],true);
 var q13 = Question('Choose the word most similar to "Trustworthy":',['Resolute', 'Tenacity', 'Relevant', 'Insolent', 'Reliable'], 'Reliable');
 
 var questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13];
 
-var $Q = $('.question') // question big box
-var $As = $('.answers')	// answers white box
-var $p = $('.p')	//	paragraph for each answer
-var $a = $('.a')	// answer
+// var $Q = $('.question') // question big box
+// var $As = $('.answers')	// answers white box
+// var $p = $('.p')	//	paragraph for each answer
+// var $a = $('.a')	// answer
 
+// Easy mode
+$('#easy').click(function(){
+	displayQuestions(6);
+});
 
-for(var i = 0; i < questions.length; i++) {
+// Medium mode
+$('#normal').click(function(){
+	displayQuestions(9);
+});
+
+// Hard mode
+$('#difficult').click(function(){
+	displayQuestions(12);
+});
+
+// Displaying questions
+function displayQuestions(num) {
+	$('#body').html('');
+	for(var i = 0; i < num; i++) {
 	var choice = '';
 	
 	$.each(questions[i].choices,function(j, e){
-			choice+= '<p class="choice" style="margin-left: 2%"> <input name="'+i+'" type="radio" class="c'+i+'" value="'+ e +'" /> <label for="c">'+e+'</label> </p>';
+			choice+= '<p class="choice" style="margin-left: 2%"> <input name="'+i+'" type="radio" id="c'+i+j+'" value="'+ e +'" /> <label for="c'+i+j+'">'+e+'</label> </p>';
 		});
 
 	var qs = '<div class="question"><p class="question1"> '+questions[i].question+'</p> <div id="'+i+'" class="answers"> '+choice+' </div> <div>  </div> </div> <hr>';
-
+	console.log(questions[i].answer)
 	$('#body').append(qs);
 }
+}
+
 
 var counter = 0;
 
+$("#btn").click(function() {
+	$('html, body').animate({
+ 	scrollTop: $("#main").offset().top
+		},0);
+	});
+
 $('#btn').click(function(){
-	if ($( "input:checked" ).length < 12){
-		alert("answer all questions wala");
+	if ($( "input:checked" ).length < questions.length){
+		alert("Answer all questions wala");
 	} else {
 		for (var i = 0; i < questions.length; i++) {
-			if ($( "input:checked" )[i].value === questions[i].answer) {
+			if ($( "input:checked" )[i].value == questions[i].answer) {
 				counter++;
 			} 
-			
 		}
-		if(counter < 70) {
-			$(body).html('<h1 style="text-align: center">Kill yourself</h1>');
+		
+		var max = questions.length;
+		var ratio = counter/max;
+		counter *= 14.5;
+		if(ratio < 0.4) {
+			$('#body').html('<h1 style="text-align: center; margin-right: 9%"> Your score is: '+counter+', Kill yourself</h1>');
 		}
-		console.log(counter);
+		else if (ratio < 0.75) {
+			$('#body').html('<h1 style="text-align: center; margin-right: 9%"> Your score is: '+counter+', You are smart</h1>');
+		}
+		else if (ratio < 0.90) {
+			$('#body').html('<h1 style="text-align: center; margin-right: 9%"> Your score is: '+counter+', You are extremely smart</h1>');
+		}
+		else {
+			$('#body').html('<h1 style="text-align: center; margin-right: 9%"> Your score is: '+counter+', You are super intelligent</h1>');
+		}
+	$('#btn').hide();
 	}
 })
 
-$(document).ready(function(){
-	$('#body').fadeIn(3000);
 
-	
-	});
+$(document).ready(function(){
+	$('#body').fadeIn(850);
+});
 
 $("#contact").click(function() {
-     	$('html, body').animate({
-         	scrollTop: $("#map").offset().top
-     		}, 3000);
- 		});
+	$('html, body').animate({
+ 	scrollTop: $("#con").offset().top
+		}, 1000);
+});
